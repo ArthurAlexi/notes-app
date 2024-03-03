01df69dc89bb2691ace2e43e353ebb0f862ea351
Arthur Alexi <arthuralexi@hotmail.com>
2024-03-03 17:40:17 -0300

    feat: initial new-note-card and note-card components

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,23 @@
+import logo from './assets/logo_notes.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
 export function App() {
-  
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} className='w-[125px] h-[50px]' alt='Notes app' />
+      <form className='w-full'>
+        <input placeholder='Search in your notes...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none' />
+      </form>
+      <div className='h-px bg-slate-700' />
+
+      <div className='grid grid-cols-3 auto-rows-[250px] gap-6 space-x-3'>
+        <NewNoteCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/new-note-card.tsx
@@ -0,0 +1,13 @@
+export function NewNoteCard() {
+    return (
+        <div className="rounded-md bg-slate-700 p-5 overflow-hidden">
+            <span className='text-sm font-medium text-slate-200'>
+                Note
+            </span>
+            <p className='text-sm leading-6 text-slate-400'>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint repellat fugit accusantium placeat vel sit
+                labore odit dolor ut perspiciatis? Alias omnis assumenda quisquam sit voluptates voluptatum ratione, nam eum.
+            </p>
+        </div>
+    )
+}
